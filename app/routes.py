4747472e26311453b1d032e5370dd7354a9e6f4b
tmonty12 <61255722+tmonty12@users.noreply.github.com>
2021-05-27 11:38:37 -0400

    added navbar and article table

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    articles = [
+        { 
+          'title': 'Jack Dorsey Says Bitcoin Can Make the World Greener. Could He Be Right?',
+          'topic': 'Bitcoin environmental concerns',
+          'url': 'https://nymag.com/intelligencer/2021/05/jack-dorsey-says-bitcoin-is-climate-friendly-is-he-right.html',
+          'time': '05/27/2021'
+        },
+        { 
+          'title': 'Dodging a bullet: Ethereum State Problems',
+          'topic': 'Ethereum state',
+          'url': 'https://blog.ethereum.org/2021/05/18/eth_state_problems/',
+          'time': '05/27/2021'
+        },
+    ]
+    return  render_template('index.html', articles=articles)
