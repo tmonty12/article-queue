7014de3ee57fb020820988e3612162eb3f4554b7
tmonty12 <61255722+tmonty12@users.noreply.github.com>
2021-06-05 14:51:00 -0400

    Added login and article creation

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,56 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, ArticleForm
+from app.models import User, Article
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
-    articles = [
-        { 
-          'title': 'Jack Dorsey Says Bitcoin Can Make the World Greener. Could He Be Right?',
-          'topic': 'Bitcoin environmental concerns',
-          'url': 'https://nymag.com/intelligencer/2021/05/jack-dorsey-says-bitcoin-is-climate-friendly-is-he-right.html',
-          'time': '05/27/2021'
-        },
-        { 
-          'title': 'Dodging a bullet: Ethereum State Problems',
-          'topic': 'Ethereum state',
-          'url': 'https://blog.ethereum.org/2021/05/18/eth_state_problems/',
-          'time': '05/27/2021'
-        },
-    ]
-    return  render_template('index.html', articles=articles)
+    form = ArticleForm()
+    if form.validate_on_submit():
+      article = Article(title=form.title.data, topic=form.topic.data, url=form.url.data, author=current_user)
+      db.session.add(article)
+      db.session.commit()
+      flash('You added a new article')
+      return redirect(url_for('index'))
+    articles = current_user.articles
+    return  render_template('index.html', form=form, articles=articles)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
   form = LoginForm()
   if form.validate_on_submit():
-    flash('Login requested for user {}, remember_me={}'.format(
-      form.username.data, form.remember_me.data
-    ))
+    user = User.query.filter_by(username=form.username.data).first()
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    next_page = request.args.get('next')
+    if not next_page or url_parse(next_page).netloc != '':
+      next_page = url_for('index')
     return redirect(url_for('index'))
   return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+  logout_user()
+  return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data, email=form.email.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('Congratulations, you are now a registered user!')
+    return redirect(url_for('login'))
+  return render_template('register.html', form=form)
